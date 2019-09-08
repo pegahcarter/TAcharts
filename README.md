7b52903cdb5498c88d7a6f5b939523c3f636b16e
Carter Carlson <carlfarterson@gmail.com>
2019-09-08 16:37:41 -0500

    Finish charting Renko and update README.md with example

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 This repository models price movement using a Renko chart.  Renko charts consist of bricks instead of candles, more clearly show market trends, and reduce the noise present on normal candlestick charts.
 
+### How it works
+
+```python
+from py.renko import Renko
+
+# NOTE: File should be a CSV of candlestick data that contains columns 'High', 'Low', and 'Close' 
+renko = Renko(<FILEPATH>)
+renko.set_brick_size()
+renko.build()
+
+renko.plot()
+```
+
+
+![png](renko.png)
+
+
+
 #### References
 * [pyrenko](https://github.com/quantroom-pro/pyrenko)
 * [ta-lib](https://github.com/mrjbq7/ta-lib)
-* [stocktrends](https://github.com/ChillarAnand/stocktrends)
--- py/renko.py
+++ py/renko.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-import time
-from datetime import datetime, timedelta
 from py.utils import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 class Renko:
     def __init__(self, FILEPATH='../data/15min.csv'):
-        self.df = pd.read_csv(FILEPATH)
+        self.df = pd.read_csv(FILEPATH)[:250]
         self.close = iter(self.df['Close'])
         self.renko = {
             'prices': [next(self.close)],
@@ -28,45 +28,77 @@ class Renko:
         ''' Helper function to get optimal brick size based on ATR '''
         if not brick_size:
             atr = average_true_range(self.df['High'], self.df['Low'], self.df['Close'], atr_period)
-            brick_size = atr.median()
+            brick_size = np.median(atr)
 
         return brick_size
 
 
     def build(self):
         ''' Create Renko data '''
-        while self.close:
-            price = next(self.close)
-            self._apply_renko(price)
-
-        return self.renko
+        [self._apply_renko(price) for price in self.close]
+        return
 
 
     def _apply_renko(self, price):
         ''' Determine if there are any new bricks to paint with current price '''
         num_bricks = 0
-        gap = (price - self.renko.prices[-1]) // self.brick_size
+        gap = (price - self.renko['prices'][-1]) // self.brick_size
         # No gap means there's not a new brick
         if gap == 0:
             return
         # Add brick(s) in the same direction
-        if (gap > 0 and self.renko.directions[-1] >= 0) \
-        or (gap < 0 and self.renko.directions[-1] <= 0):
+        if (gap > 0 and self.renko['directions'][-1] >= 0) \
+        or (gap < 0 and self.renko['directions'][-1] <= 0):
             num_bricks = gap
         # Gap >= 2 or -2 and opposite renko direction means we're switching brick direction
         elif abs(gap) >= 2:
             num_bricks = gap - np.sign(gap)
             self._update_renko(gap, 2)
 
-        return [self._update_renko(gap) for brick in range(num_bricks)]
+        for brick in range(int(abs(num_bricks))):
+            self._update_renko(gap)
+
+        return self.renko
 
 
     def _update_renko(self, gap, brick_multiplier=1):
         ''' Append price and new block to renko dict '''
-        renko_price = self.renko.prices[-1] + brick_multiplier*self.brick_size*np.sign(gap)
-        self.renko.prices.append(renko_price)
-        self.renko.directions.append(np.sign(gap))
+        renko_price = self.renko['prices'][-1] + brick_multiplier*self.brick_size*np.sign(gap)
+        self.renko['prices'].append(renko_price)
+        self.renko['directions'].append(np.sign(gap))
+        return
+
+
+    def plot(self):
+        fig, ax = plt.subplots(1, figsize=(20,10))
+        ax.set_title("Renko Chart (brick size = {})".format(round(self.brick_size, 2)))
+        ax.set_ylabel('Price')
+
+        # Calculate range for axis
+        ax.set_xlim(0, len(self.renko['prices'])+1)
+        ax.set_ylim(min(self.renko['prices']) - 2*self.brick_size, max(self.renko['prices']) + 2*self.brick_size)
+
+        # Add bricks to chart
+        for x in range(1, len(self.renko['prices'])):
+            price, direction = map(lambda val: val[x], self.renko.values())
+            if direction == 1:
+                facecolor='g'
+                y = price - self.brick_size
+            else:
+                facecolor='r'
+                y = price
+
+            ax.add_patch(
+                patches.Rectangle(
+                    (x, y),
+                    height=self.brick_size,
+                    width=1,
+                    facecolor=facecolor
+                )
+            )
+
+        return plt.show()
 
 
     def __repr__(self):
-        return f"{self.renko}"
+        return self.renko
