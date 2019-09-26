68219ba495b5f9cfd391016544dca69aa73ddcb4
Carter Carlson <carlfarterson@gmail.com>
2019-09-25 19:40:47 -0500

    Standardize painting of renko blocks

--- py/renko.py
+++ py/renko.py
@@ -8,18 +8,13 @@ import matplotlib.patches as patches
 class Renko:
     def __init__(self, df):
         try:
-            self.date = df['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+            self.date = df['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')).tolist()
         except:
-            self.date = df['date']
+            self.date = df['date'].tolist()
 
-        self.high = df['high']
-        self.low = df['low']
-        self.close = df['close']
-        self.renko = {
-            'date': [self.date[0]],
-            'price': [self.close[0]],
-            'direction': [0]
-        }
+        self.high = df['high'].tolist()
+        self.low = df['low'].tolist()
+        self.close = df['close'].tolist()
 
 
     def set_brick_size(self, auto=True, brick_size=None, atr_period=14):
@@ -33,7 +28,7 @@ class Renko:
 
     def _optimize_brick_size(self, auto, brick_size, atr_period):
         ''' Helper function to get optimal brick size based on ATR '''
-        if auto:
+        if auto and not brick_size:
             atr = average_true_range(self.high, self.low, self.close, atr_period)
             brick_size = np.median(atr)
 
@@ -42,6 +37,14 @@ class Renko:
 
     def build(self):
         ''' Create Renko data '''
+        units = self.close[0] // self.brick_size
+        start_price = units * self.brick_size
+
+        self.renko = {
+            'date': [self.date[0]],
+            'price': [start_price],
+            'direction': [0]
+        }
         for date, price in zip(self.date[1:], self.close[1:]):
             self._apply_renko(date, price)
         return self.renko.values()
@@ -51,30 +54,31 @@ class Renko:
         ''' Determine if there are any new bricks to paint with current price '''
         num_bricks = 0
         gap = (price - self.renko['price'][-1]) // self.brick_size
+        direction = np.sign(gap)
         # No gap means there's not a new brick
-        if gap == 0:
+        if direction == 0:
             return
         # Add brick(s) in the same direction
         if (gap > 0 and self.renko['direction'][-1] >= 0) \
         or (gap < 0 and self.renko['direction'][-1] <= 0):
             num_bricks = gap
         # Gap >= 2 or -2 and opposite renko direction means we're switching brick direction
-        elif abs(gap) >= 2:
-            num_bricks = gap - np.sign(gap)
-            self._update_renko(date, gap, 2)
+        elif np.abs(gap) >= 2:
+            num_bricks = gap - 2*direction
+            self._update_renko(date, direction, 2)
 
-        for brick in range(int(abs(num_bricks))):
-            self._update_renko(date, gap)
-
+        for brick in range(abs(int(num_bricks))):
+            self._update_renko(date, direction)
+            
         return self.renko
 
 
-    def _update_renko(self, date, gap, brick_multiplier=1):
+    def _update_renko(self, date, direction, brick_multiplier=1):
         ''' Append price and new block to renko dict '''
-        renko_price = self.renko['price'][-1] + brick_multiplier*self.brick_size*np.sign(gap)
+        renko_price = self.renko['price'][-1] + (direction * brick_multiplier * self.brick_size)
         self.renko['date'].append(date)
         self.renko['price'].append(renko_price)
-        self.renko['direction'].append(np.sign(gap))
+        self.renko['direction'].append(direction)
         return
 
 
