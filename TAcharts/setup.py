da0ff8c9afbdb8c13a720de605a12b4f5f65a58c
Carter Carlson <carlfarterson@gmail.com>
2020-01-07 09:52:17 -0600

    Finish rough version of setup.py

--- TAcharts/setup.py
+++ TAcharts/setup.py
@@ -2,33 +2,33 @@
 # -*- coding: utf-8; py-indent-offset:4 -*-
 
 import os.path
-import codecs  # To use a consistent encoding
+import codecs
 import setuptools
 
-here = os.path.abspath(os.path.dirname(__file__))
-
 # Get the long description from the relevant file
-with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-# Package name
-pname = 'TAcharts'
-
-# Get the version
-version = 0.1
-
-# Generate links
-gurl = 'https://github.com/carlfarterson/' + pname
-download_url = gurl + '/tarball/' + __version__
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+    long_description = readme.read()
 
 
 setuptools.setup(
-    name=pname,
-    version=version,
-
-    description='TA Charting tool',
-    long_description = long_description,
-
-
+  name = 'TAcharts',
+  packages = ['TAcharts'],
+  version = '0.0.1',
+  author = 'Carter Carlson',
+  author_email = 'carlfarterson@gmail.com',
+  license = 'MIT',
+  description = 'TA Charting tool',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url = 'https://github.com/carlfarterson/TAcharts',
+  packages=setuptools.find_packages(),
+  download_url = 'https://github.com/carlfarterson/TAcharts/archive/v_01.tar.gz',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers'
+  ],
+  python_requires='>=3.6'
 )
