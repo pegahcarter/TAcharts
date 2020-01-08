6633cd1aa1092d19ba5dedc3fd4656fbebeb2d7a
Carter Carlson <carlfarterson@gmail.com>
2020-01-08 00:58:01 -0600

    Work on imports within __init__.py

--- TAcharts/__init__.py
+++ TAcharts/__init__.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
 
-from . import utils as utils
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .utils import num2date, date2num, time2num, num2time
-
-from .store import Store
-from . import broker as broker
-from .timer import *
 from . import utils as utils
 
-from . import feeds as feeds
+from .wrappers import * 
+
 from . import indicators as indicators
-
-
-
-# Load contributed indicators and studies
-import backtrader.indicators.contrib
-import backtrader.studies.contrib
--- TAcharts/plot/__init__.py
+++ TAcharts/plot/__init__.py
@@ -2,3 +2,5 @@ from .bollinger import *
 from .ichimoku import *
 from .charts import *
 from .renko import *
+
+from .plot import Plot
