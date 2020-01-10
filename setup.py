0bb4e4122ad2322e38fd8d275fe318a85b06a683
Carter Carlson <carlfarterson@gmail.com>
2020-01-09 19:05:41 -0600

    Update setup.py to 0.0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as readme:
 
 setuptools.setup(
   name = 'TAcharts',
-  version = '0.0.7',
+  version = '0.0.8',
   author = 'Carter Carlson',
   author_email = 'carlfarterson@gmail.com',
   license = 'MIT',
