e049b1921310ced049c1e3827f846e8c5f270848
Carter Carlson <carlfarterson@gmail.com>
2020-03-25 19:07:55 -0500

    Update to v0.0.18

--- setup.py
+++ setup.py
@@ -12,23 +12,42 @@ with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as readme:
 
 
 setuptools.setup(
-  name = 'TAcharts',
-  version = '0.0.17',
-  author = 'Carter Carlson',
-  author_email = 'carlfarterson@gmail.com',
-  license = 'MIT',
-  description = 'TA Charting tool',
+
+  name='TAcharts',
+
+  version='0.0.18',
+
+  author='Carter Carlson',
+
+  author_email='carlfarterson@gmail.com',
+
+  license='MIT',
+
+  description='TA Charting tool',
+
   keywords=['TA', 'mathematics', 'algorithms'],
+
   long_description=long_description,
+
   long_description_content_type='text/markdown',
-  url = 'https://github.com/carlfarterson/TAcharts',
+
+  url='https://github.com/carlfarterson/TAcharts',
+
   packages=setuptools.find_packages(),
-  download_url = 'https://github.com/carlfarterson/TAcharts/archive/v_0.0.1.tar.gz',
+
+  install_requires=[
+    'pandas>=0.21.0',
+    'numpy>=1.0.0',
+    'matplotlib>=2.0.0'
+  ],
+
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
     'Intended Audience :: Developers'
   ],
+
   python_requires='>=3.6'
+
 )
