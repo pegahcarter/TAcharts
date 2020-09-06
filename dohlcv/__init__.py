44d6b314854e985cda0a5a68a09068bd4278054e
Carl Farterson <carlfarterson@pm.me>
2020-09-06 00:53:14 -0700

    Delete old dohlcv directory

--- dohlcv/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8; py-indent-offset:4 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import pandas as pd
-import os
