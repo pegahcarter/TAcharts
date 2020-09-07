6d288e2c29730576f138f39847c089760e0cec4e
Carl Farterson <carlfarterson@pm.me>
2020-09-07 09:15:28 -0700

    Move variable declarations outside class

--- TAcharts/utils/ohlcv.py
+++ TAcharts/utils/ohlcv.py
@@ -6,20 +6,19 @@ import os
 import pandas as pd
 
 DOWNLOAD_URL = "https://raw.githubusercontent.com/carlfarterson/TAcharts/master/ohlcv"
+COINS = ["btc", "eth", "ethusd", "xrp", "ltc", "eos", "ada"]
+BASEPATH = TAcharts.__path__[0] + "/ohlcv"
 
 
 class OHLCV:
 
-    coins = ["btc", "eth", "ethusd", "xrp", "ltc", "eos", "ada"]
-
     # Check if we have already pulled the price data and saved it locally
     def __init__(self, usecols=None):
-
-        self.basepath = TAcharts.__path__[0] + "/ohlcv"
 
         # If we have not yet saved OHLCV prices locally, fetch them from github
         if "ohlcv" not in os.listdir(self.basepath):
             self.fetch_OHLCV()
+
         # Now that we have OHLCV prices saved, fetch them and set themn to ourself
         for coin in self.coins:
             df = pd.read_csv(f"{self.basepath}/{coin}.csv")
